16c25b257dfd20a62f1bbf9f0b871cc71acc36de
Fr1k1 <martin.friscic6@gmail.com>
2022-09-10 16:34:30 +0200

    Finished sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,13 @@
 import "./Sidebar.scss";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../../other/images/logo.jpg";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHouseUser} from "@fortawesome/free-solid-svg-icons";
-import {faUserNinja} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouseUser } from "@fortawesome/free-solid-svg-icons";
+import { faUserNinja } from "@fortawesome/free-solid-svg-icons";
 import { faIdCard } from "@fortawesome/free-solid-svg-icons";
-
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => {
   return (
@@ -15,21 +17,53 @@ const Sidebar = () => {
       </Link>
 
       <nav>
-        <NavLink exact="true" activeclassname="active" className="home-link" to="/home">
-        <FontAwesomeIcon icon={faHouseUser} color="#4d4d4e"/>
-
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="home-link"
+          to=""
+        >
+          <FontAwesomeIcon icon={faHouseUser} color="#4d4d4e" />
         </NavLink>
 
-        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-        <FontAwesomeIcon icon={faUserNinja} color="#4d4d4e"  />
-
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUserNinja} color="#4d4d4e" />
         </NavLink>
 
-        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-        <FontAwesomeIcon icon={faIdCard} color="#4d4d4e"/>
-
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faIdCard} color="#4d4d4e" />
         </NavLink>
       </nav>
+
+      <ul>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://github.com/Fr1k1">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/martin-fri%C5%A1%C4%8Di%C4%87-6164ab202/">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://www.instagram.com/martinfriscic/">
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
